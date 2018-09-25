bb27e905358b56673ccb63813a8bb2e175fd130c
saurabh-1995 <singhsaurabhj264@gmail.com>
2018-09-25 09:45:48 +0530

    firstChanges

---
+++ JWTApplication/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTApplication.Controllers
+{
+    
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        [HttpPost("token")]
+        public IActionResult Token()
+        {
+            var header = Request.Headers["Authorization"];
+            if(header.ToString().StartsWith("Basic"))
+            {
+                var credValue = header.ToString().Substring("Basic".Length).Trim();
+                var usernameAndPassenc = Encoding.UTF8.GetString(Convert.FromBase64String(credValue));
+                var usernameAndPass = usernameAndPassenc.Split(":");
+                //string tokenString = "test";
+                if (usernameAndPass[0] == "Admin" && usernameAndPass[1] == "password")
+                {
+                    var claimsdata = new[] { new Claim(ClaimTypes.Name, usernameAndPass[0]) };
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("njdsjdsjbdshbdshysgdsgdsjnashshjtsdgbxgdbdgdbdhasd"));
+                    var signInCred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+                    var token = new JwtSecurityToken(
+                    issuer: "mysite.com",
+                    audience: "mysite.com",
+                    expires: DateTime.Now.AddMinutes(1),
+                    claims: claimsdata,
+                    signingCredentials: signInCred
+                );
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                    return Ok(tokenString);
+                }
+            }
+            return BadRequest("Wrong Request");
+            
+            //return View();
+        }
+    }
+}
---
+++ JWTApplication/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer=true,
+                    ValidateAudience=true,
+                    ValidateIssuerSigningKey=true,
+                    ValidIssuer="mysite.com",
+                    ValidAudience="mysite.com",
+                    IssuerSigningKey= new SymmetricSecurityKey(Encoding.UTF8.GetBytes("njdsjdsjbdshbdshysgdsgdsjnashshjtsdgbxgdbdgdbdhasd"))
+            };
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
